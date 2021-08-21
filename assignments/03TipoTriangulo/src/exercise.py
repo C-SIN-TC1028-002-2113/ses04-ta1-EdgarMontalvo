6cd7fad147874f0285e5f54d6fe807085453fd58
EdgarMontalvo <A01742343@itesm.mx>
2021-08-21 01:08:06 +0000

    lol

--- assignments/03TipoTriangulo/src/exercise.py
+++ assignments/03TipoTriangulo/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    lado1 = int(input("Ingresa la medida del lado 1: "))
+    lado2 = int(input("Ingresa la medida del lado 2: "))
+    lado3 = int(input("Ingresa la medida del lado 3: "))
+
+    if lado1 
 
 if __name__=='__main__':
     main()
--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,6 +1,24 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    peso = float(input("Peso en kg: "))
+    altura = float(input("Altura en m: "))
+
+    indice =float(peso / altura**2)
+    
+    if indice == 0:
+        print("Revisa tus datos, alguno de ellos es erróneo")
+    elif indice <20:
+        print("PESO BAJO")
+    elif 20 <= indice and indice < 25:
+        print("NORMAL")
+    elif 25<= indice and indice <30:
+        print("SOBREPESO")
+    elif 30<= indice and indice< 40:
+        print("OBESIDAD")
+    elif indice >= 40:
+        print("OBESIDAD MORBIDA")
+    else:
+        print("Revisa tus datos, alguno de ellos es erróneo")
 
 if __name__=='__main__':
     main()
