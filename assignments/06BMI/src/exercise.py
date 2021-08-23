2c403d349edbd214941ca38c24685523042659ee
EdgarMontalvo <A01742343@itesm.mx>
2021-08-23 00:01:03 +0000

    IMC else sale mal por alguna razon

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -2,21 +2,21 @@ def main():
     #escribe tu código abajo de esta línea
     peso = float(input("Peso en kg: "))
     altura = float(input("Altura en m: "))
-    if altura > 0 and peso > 0:
-          indice = (peso / altura**2)
-          if indice <20:
+    if  peso > 0 and altura > 0:
+          IMC = (peso/altura**2)
+          if IMC < 20:
                print("PESO BAJO")
-          elif 20 <= indice and indice < 25:
-               print("NORMAL")
-          elif 25<= indice and indice <30:
+          elif IMC >= 20 and IMC < 25:
+               print ("NORMAL")
+          elif IMC >= 25 and IMC < 30:
                print("SOBREPESO")
-          elif 30<= indice and indice< 40:
+          elif IMC >= 30 and IMC < 40:
                print("OBESIDAD")
-          else:
-               print("OBESIDAD MORBIDA")
-    else:
-        print("Revisa tus datos, alguno de ellos es erróneo")
-
+          else: 
+               print ("OBESIDAD MORBIDA")
+     else:
+          print("Revisa tus datos, alguno de ellos es erróneo.")
+          
 
 if __name__=='__main__':
     main()
